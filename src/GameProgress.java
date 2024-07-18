109dd55c617954afadc0d36ada4e740800eb145e
Anton Santalov <anton.santalov1995@gmail.com>
2024-07-18 12:00:29 +0400

    Code added.

--- src/GameProgress.java
+++ src/GameProgress.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class GameProgress {
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class GameProgress implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final int health;
+    private final int weapons;
+    private final int lvl;
+    private final double distance;
+
+    public GameProgress(int health, int weapons, int lvl, double distance) {
+        this.health = health;
+        this.weapons = weapons;
+        this.lvl = lvl;
+        this.distance = distance;
+    }
+
+    static void createDirectories(String path, StringBuilder log) {
+        File dir = new File(path);
+        if (!dir.exists()) {
+            if (dir.mkdirs()) {
+                log.append("Directory '").append(path).append("' created successfully.\n");
+            } else {
+                log.append("Failed to create directory '").append(path).append("'.\n");
+            }
+        } else {
+            log.append("Directory '").append(path).append("' already exists.\n");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "GameProgress{" +
+            "health=" + health +
+            ", weapons=" + weapons +
+            ", lvl=" + lvl +
+            ", distance=" + distance +
+            '}';
+    }
+
+    public static void saveGame(String filePath, GameProgress gameProgress) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(filePath);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        }
+    }
+
+    public static void zipFiles(String zipFilePath, List<String> filesToZip) throws IOException {
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFilePath))) {
+            for (String file : filesToZip) {
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    ZipEntry zipEntry = new ZipEntry(new File(file).getName());
+                    zos.putNextEntry(zipEntry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zos.write(buffer);
+                    zos.closeEntry();
+                }
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder log = new StringBuilder();
+        List<String> filesToZip = new ArrayList<>();
+
+        GameProgress gameProgress1 = new GameProgress(100, 2, 1, 0.0);
+        GameProgress gameProgress2 = new GameProgress(50, 1, 2, 10.5);
+        GameProgress gameProgress3 = new GameProgress(200, 3, 3, 20.0);
+
+        String saveDir = "Games/GunRunner/savegames";
+        GameProgress.createDirectories(saveDir, log);
+
+        String zipFilePath = "Games/GunRunner/savegames/zip.zip";
+
+        GameProgress.saveGame(saveDir + "save1.dat", gameProgress1);
+        GameProgress.saveGame(saveDir + "save2.dat", gameProgress2);
+        GameProgress.saveGame(saveDir + "save3.dat", gameProgress3);
+
+        filesToZip.add(saveDir + "save1.dat");
+        filesToZip.add(saveDir + "save2.dat");
+        filesToZip.add(saveDir + "save3.dat");
+
+        GameProgress.zipFiles(zipFilePath, filesToZip);
+
+        for (String file : filesToZip) {
+            File saveFile = new File(file);
+            if (saveFile.exists()) {
+                saveFile.delete();
+            }
         }
     }
 }
